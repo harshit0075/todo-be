be976b5c98cbc52d2e2f66e7153c0d6a87818233
masaiharshit <harshit007.ara@gmail.com>
2024-03-17 12:56:23 +0530

    todo-be-done

---
+++ README.md
@@ -0,0 +1,14 @@
+install all node modules.
+
+To run the server npm run server 
+
+once the server start running you can perform task at the deployed link-
+
+
+for adding a post : http://localhost:8080/taskRouter/add
+
+for getting a data: http://localhost:8080/taskRouter/
+
+for updating the data :  http://localhost:8080/taskRouter//update/:taskId"
+
+for deleting a data : http://localhost:8080/taskRouter//delete/:taskId"
---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const connection  = require("./config/db");
+const cors=require("cors")
+require("dotenv").config()
+
+const {taskRouter} = require("./routes/task");
+
+app.use(cors())
+app.use(express.json());
+app.use("/taskRouter", taskRouter);
+
+
+
+app.listen(8080, async () => {
+    try {
+        await connection;
+        console.log("Connected to the database");
+        
+    } catch (err) {
+        console.error("Error connecting to the database:", err);
+    }
+    console.log("Server started on port 8080");
+});
+
